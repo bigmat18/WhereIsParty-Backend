10a5d281d4ec6e391734ccdbb52be96bbf3def38
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-10-09 18:17:33 +0200

    Adding event retrieve endpoints [not tested]

--- routers/event.py
+++ routers/event.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Form, File, UploadFile
 from sqlalchemy.orm import Session
 from schemas.event import EventSchema
-from models import User, Organization, Event, Location
+from models import User, ReferralLink, Event, Location
 from database import get_db
 from utils.file_manager import delete_file, upload_file, AWS_BUCKET_URL
 from utils.get_current_user import get_current_user
@@ -10,12 +10,14 @@ from .organization import get_organization
 
 event_router = APIRouter(tags=["Event"])
 
+
 def get_event(id:str, db:Session) -> Event:
     event = db.query(Event).filter(Event.id == id).first()
     if not event: 
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f"Non esiste una event con questo id")
     return event
+
 
 @event_router.post(path="/organization/{id_organization}/events", status_code=status.HTTP_201_CREATED)
 def event_create(id_organization: str,
@@ -45,6 +47,20 @@ def event_create(id_organization: str,
     db.add(event)
     db.commit()
     db.refresh(event)
+    
+    return event
+
+
+@event_router.get(path="/event/{id_event}", status_code=status.HTTP_200_OK, response_model=EventSchema)
+def event_retrieve(id_event: str,
+                   id_referral: str,
+                   user: User = Depends(get_current_user),
+                   db: Session = Depends(get_db)):
+    if db.query(ReferralLink).filter(ReferralLink.id == id_referral):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Refferal link invalido")
+        
+    event = get_event(id_event, db)
     
     return event
 
