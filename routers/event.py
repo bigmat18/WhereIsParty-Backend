7895b6a96ba3349e46d8107485a9d0ac86f86d10
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-10-10 23:44:32 +0200

    Remove patch endpoints

--- routers/event.py
+++ routers/event.py
@@ -64,64 +64,64 @@ def event_retrieve(id_event: str,
     return event
 
 
-@event_router.patch(path="/event/{id_event}", status_code=status.HTTP_200_OK, response_model=EventSchema)
-def event_update(id_event: str,
-                 event_data: EventSchema,
-                 user: User = Depends(get_current_user),
-                 db: Session = Depends(get_db)):
-    event = get_event(id_event, db)
+# @event_router.patch(path="/event/{id_event}", status_code=status.HTTP_200_OK, response_model=EventSchema)
+# def event_update(id_event: str,
+#                  event_data: EventSchema,
+#                  user: User = Depends(get_current_user),
+#                  db: Session = Depends(get_db)):
+#     event = get_event(id_event, db)
     
-    if event_data.name:
-        event.name = event_data.name
+#     if event_data.name:
+#         event.name = event_data.name
         
-    if event_data.description:
-        event.description = event_data.description
+#     if event_data.description:
+#         event.description = event_data.description
 
-    if event_data.date:
-        event.date = event_data.date
+#     if event_data.date:
+#         event.date = event_data.date
         
-    if event_data.open_date:
-        event.open_date = event_data.open_date
+#     if event_data.open_date:
+#         event.open_date = event_data.open_date
         
-    if event_data.close_date:
-        event.close_date = event_data.close_date
+#     if event_data.close_date:
+#         event.close_date = event_data.close_date
 
-    if event_data.visible_date:
-        event.visible_date = event_data.visible_date
+#     if event_data.visible_date:
+#         event.visible_date = event_data.visible_date
 
-    if event_data.primary_color:
-        event.primary_color = event_data.primary_color
+#     if event_data.primary_color:
+#         event.primary_color = event_data.primary_color
         
-    if event_data.secondary_color:
-        event.secondary_color = event_data.secondary_color
+#     if event_data.secondary_color:
+#         event.secondary_color = event_data.secondary_color
         
-    if event_data.location:
-        location = db.query(Location).filter(Location.id == event_data.location).first()
-        if location: event.location = location.id
+#     if event_data.location:
+#         location = db.query(Location).filter(Location.id == event_data.location).first()
+#         if location: event.location = location.id
 
-    db.commit()
-    db.refresh(event)
+#     db.commit()
+#     db.refresh(event)
 
-    return event
+#     return event
 
 
-@event_router.patch(path="/event/{id_event}/image", status_code=status.HTTP_200_OK)
-def event_image_update(id_event: str,
-                       image: UploadFile = File(),
-                       user: User = Depends(get_current_user),
-                       db: Session = Depends(get_db)):
+# @event_router.patch(path="/event/{id_event}/image", status_code=status.HTTP_200_OK)
+# def event_image_update(id_event: str,
+#                        image: UploadFile = File(),
+#                        user: User = Depends(get_current_user),
+#                        db: Session = Depends(get_db)):
     
-    event = get_event(id_event, db)
+#     event = get_event(id_event, db)
     
-    url = upload_file(image.file, f"/event-{event.id}", "event-images")
+#     url = upload_file(image.file, f"/event-{event.id}", "event-images")
     
-    if url and event.image_url:
-        path = event.image_url.replace(AWS_BUCKET_URL + "/", '')
-        try: delete_file(path)
-        except: print("Eliminazione vecchio file non riuscita")
+#     if url and event.image_url:
+#         path = event.image_url.replace(AWS_BUCKET_URL + "/", '')
+#         try: delete_file(path)
+#         except: print("Eliminazione vecchio file non riuscita")
         
-    event.image_url = url
-    db.commit()
-    db.refresh(event)
+#     event.image_url = url
+#     db.commit()
+#     db.refresh(event)
         
-    return {"image_url": url}
+#     return {"image_url": url}
