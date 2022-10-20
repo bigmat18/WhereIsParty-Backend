8e13e589299670fadfe81ce6c5523284aebd2331
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-10-20 15:30:22 +0200

    Fixo il casino che il morozzi mi lascia

--- schemas/booking.py
+++ schemas/booking.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-from pydantic import UUID4, BaseModel, EmailStr
+from pydantic import UUID3, UUID4, UUID5, BaseModel, EmailStr, UUID1
 from typing import Union
 from .organization import UserSchema, ReferralLinkSchema
 from .event import EventSchema
 
 class BookingSchema(BaseModel):
-    id: UUID4 = None
+    id: Union[UUID4, UUID1, UUID3, UUID5] = None
     user: UserSchema = None
     event: EventSchema = None
-    referral_link: ReferralLinkSchema | None | str = None
+    referral_link: Union[ReferralLinkSchema, None, str] = None
     date_booked: datetime = None
     code: str = None
     entered: bool = None
