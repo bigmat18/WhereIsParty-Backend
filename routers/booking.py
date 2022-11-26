1a54a734b012d4c273cbf8f516bb5f41e863f145
Matteo Giuntoni <59317264+Matteo181202@users.noreply.github.com>
2022-11-27 00:06:18 +0100

    Fix refferral link in booking post endpoint

--- routers/booking.py
+++ routers/booking.py
@@ -51,6 +51,7 @@ def booking_list(id_event: str,
 @booking_router.post(path="/event/{id_event}/booking", status_code=status.HTTP_201_CREATED, response_model=BookingSchema)
 def booking_create(id_event: str,
                    booking: BookingSchema,
+                   referral_link: str,
                    user: User = Depends(get_current_user),
                    db: Session = Depends(get_db)):
     event = get_event(id_event, db)
@@ -62,16 +63,17 @@ def booking_create(id_event: str,
     code = generate_random_string()
     while db.query(Booking).filter(and_(Booking.code == code, Booking.id_event == id_event)).first():
         code = generate_random_string()
+        
+    booking = None
     
-    if booking.referral_link:
+    if referral_link:
         referral_link = db.query(ReferralLink)\
-                        .filter(and_(ReferralLink.name == booking.referral_link, ReferralLink.id_organization == event.id_organization))\
+                        .filter(and_(ReferralLink.name == referral_link, ReferralLink.id_organization == event.id_organization))\
                         .first()
         if referral_link:
             booking = Booking(id_user=user.id, id_event=event.id, code=code, id_referral_link=referral_link.id)
-        else: 
-            booking = Booking(id_user=user.id, id_event=event.id, code=code)
-    else:
+
+    if not booking: 
         booking = Booking(id_user=user.id, id_event=event.id, code=code)
     
     db.add(booking)
